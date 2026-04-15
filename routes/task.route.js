52b326d82d9bcf798521bdda449266bfb01ba698
chistisaicharan <chistisaicharan@gmail.com>
2026-04-15 15:09:33 +0530

    refactor: remove separate filter/search controllers and unify logic into getTasks API

--- routes/task.route.js
+++ routes/task.route.js
@@ -4,21 +4,17 @@ const express=require("express")
 
 const routes=express.Router();
 
-const {createTask,getTasks,updateTasks,deleteTask,taskFiltering,searchTask}=require("../controllers/task.controller");
+const {createTask,getTasks,updateTasks,deleteTask}=require("../controllers/task.controller");
 
 const authMiddleware=require("../middlewares/auth.middleware");
 
 routes.post("/createTask",authMiddleware,createTask)
 
-routes.get("/",getTasks)
+routes.get("/task",getTasks)
 
 routes.patch("/task/:id",authMiddleware,updateTasks)
 routes.delete("/delete/:id",authMiddleware,deleteTask)
 
-// filter and search
-routes.get("/task",authMiddleware,taskFiltering)
-
-routes.get("/task",authMiddleware,searchTask)
 
 
 module.exports=routes
